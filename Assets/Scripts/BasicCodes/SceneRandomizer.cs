e5008a4ba3aae847ff6fe3b7f360ead45ea4642d
aoi00115 <104622250+aoi00115@users.noreply.github.com>
2023-08-23 04:55:34 +0900

    CorrectAnimation and Question/Choices for Stage1~Stage4 added

---
+++ Assets/Scripts/BasicCodes/SceneRandomizer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class SceneRandomizer : MonoBehaviour
+{
+    public static List<string> QuestionScenes = new List<string>(){
+        "Stage1",
+        "Stage2",
+        "Stage3",
+        "Stage4"
+    };
+    public Button SelectButton;
+    private static int RandomIndex;
+    private static string SelectedSceneName;
+
+
+    private void Update()
+    {
+        foreach (string Question in QuestionScenes)
+        {
+            Debug.Log(Question);
+        }
+    }
+
+    public void SelectRandomScene()
+    {
+        if (QuestionScenes.Count > 0)
+        {
+            RandomIndex = Random.Range(0, QuestionScenes.Count);
+            SelectedSceneName = QuestionScenes[RandomIndex];
+            SceneManager.LoadScene(SelectedSceneName);
+
+        }
+        else
+        {
+            SceneManager.LoadScene("res");
+        }
+        QuestionScenes.RemoveAt(RandomIndex);
+    }
+
+}
+
+//int a = questionScenes.RemoveAll(x => x.Equals(sceneRandom));
+//var sceneRandom = questionScenes[Random.Range(0, questionScenes.Count)];
--- Assets/Scripts/CameraBehaviourandAnimation/GameManager.cs
+++ Assets/Scripts/CameraBehaviourandAnimation/GameManager.cs
@@ -3,8 +3,12 @@ using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,14 +17,14 @@ public class GameManager : MonoBehaviour
     public float timer = 0.0f;
 
     [Header("Stage Setting")]
-    public bool answer;                 // Change this variable in the inspector for every question
+    public bool answer; 
+    public bool title;                // Change this variable in the inspector for every question
     public bool stage1;
     public bool stage2;
     public bool stage3;
     public bool stage4;
-
-    public static int scoreCounter = 0; 
     public bool correctChecker;
+    public static int scoreCounter = 0; 
 
     bool isAnswered = false;
 
@@ -29,65 +33,120 @@ public class GameManager : MonoBehaviour
     public GameObject pivotPoint;
     public PostProcessVolume postProcessingVolume;
     public ChromaticAberration chromaticAberration;
+    public CanvasGroup questionCG;
+    public RectTransform questionRT;
+    public CanvasGroup correctCG;
+    public RectTransform correctRT;
+    public CanvasGroup wrongCG;
+    public RectTransform wrongRT;
+    public Vector2 correctLocalPos;
+    public Vector2 wrongLocalPos;
+
+    public static List<string> QuestionScenes = new List<string>(){
+        "Stage1",
+        "Stage2",
+        "Stage3",
+        "Stage4"
+    };
+    static int RandomIndex;
+    static string SelectedSceneName;
 
     // Start is called before the first frame update
     void Start()
     {
-        car = GameObject.Find("Car");
-        pivotPoint = GameObject.Find("Car/PivotPoint");
+        if(!title)
+        {
+            car = GameObject.Find("Car");
+            pivotPoint = GameObject.Find("Car/PivotPoint");
 
+            questionCG = GameObject.Find("Canvas/Question").GetComponent<CanvasGroup>();
+            questionRT = GameObject.Find("Canvas/Question").GetComponent<RectTransform>();
+            correctCG = GameObject.Find("Canvas/Correct").GetComponent<CanvasGroup>();
+            correctRT = GameObject.Find("Canvas/Correct").GetComponent<RectTransform>();
+            wrongCG = GameObject.Find("Canvas/Wrong").GetComponent<CanvasGroup>();
+            wrongRT = GameObject.Find("Canvas/Wrong").GetComponent<RectTransform>();
+            questionCG.alpha = 0f;
+            correctCG.alpha = 0f;
+            wrongCG.alpha = 0f;
+            
+            // Storing the original position of correct/wrong prompt in variables
+            correctLocalPos = correctRT.transform.localPosition;
+            wrongLocalPos = wrongRT.transform.localPosition;
+        }
+        
         postProcessingVolume = GameObject.Find("PostProcessing").GetComponent<PostProcessVolume>();
         postProcessingVolume.profile.TryGetSettings(out chromaticAberration);
-
-        Time.timeScale = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if(!isAnswered)
+        if(!title)
         {
-            // Checing if the user input is correct
-            if(Input.GetKeyDown(KeyCode.O))
+            timer += Time.deltaTime;
+
+            if(!isAnswered && timer > 3.25f)
             {
-                chromaticAberration.intensity.value = 0;
-                correctChecker = true;
-                isAnswered = true;
-            }
-            if(Input.GetKeyDown(KeyCode.X))
-            {
-                chromaticAberration.intensity.value = 0;
-                correctChecker = false;
-                isAnswered = true;
+                // Checing if the user input is correct
+                if(Input.GetKeyDown(KeyCode.O))
+                {
+                    chromaticAberration.intensity.value = 0;
+                    correctChecker = true;
+                    isAnswered = true;
+                    PromptFadeOut();
+                }
+                if(Input.GetKeyDown(KeyCode.X))
+                {
+                    chromaticAberration.intensity.value = 0;
+                    correctChecker = false;
+                    isAnswered = true;
+                    PromptFadeOut();
+                }
             }
         }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                SelectRandomScene();
+            }
+        } 
 
         // Debug
         // Debug.Log(speed);
         // Debug.Log(timer);
         // Debug.Log(chromaticAberration.intensity.value);
+        // foreach (string Question in QuestionScenes)
+        // {
+        //     Debug.Log(Question);
+        // }
     }
 
     void FixedUpdate()
     {
-        if(!isAnswered)
+        if(!title)
         {
-            PreAnswerAnimation();
-        }
-        else
-        {
-            if(correctChecker == answer)    // Play the correct animation
+            QuestionFadeIn();
+            ChoiceFadeIn();
+
+            if(!isAnswered)                     // If not answered
             {
-                CorrectAnimation();       
-                scoreCounter++;
+                PreAnswerAnimation();
             }
-            else                            // Play the wrong animation
+            else                                // If answered
             {
-                WrongAnimation();           
+                if(correctChecker == answer)    // Play the correct animation
+                {
+                    CorrectAnimation();       
+                    scoreCounter++;
+                }
+                else                            // Play the wrong animation
+                {
+                    WrongAnimation();           
+                }
+                Invoke("SelectRandomScene", 5.0f); // Changing the scene and resetting everything
             }
-        }
+        }        
     }
 
     void PreAnswerAnimation()
@@ -116,10 +175,12 @@ public class GameManager : MonoBehaviour
 
     void CorrectAnimation()
     {
+        speed = 3.0f;
+        car.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+
         if(stage1)
         {
-            speed = 3.0f;
-            car.transform.position += new Vector3(0, 0, speed * Time.deltaTime);
+            
         }
         if(stage2)
         {
@@ -155,5 +216,65 @@ public class GameManager : MonoBehaviour
             
         }
         Debug.Log("WrongAnswer");
+    }
+
+    void QuestionFadeIn()
+    {
+        if(timer > 1f && timer < 2f)
+        {
+            questionRT.transform.localPosition = new Vector3(0f, 100f, 0f);
+            questionRT.DOAnchorPos(new Vector2(0f, 0f), 0.25f, false).SetEase(Ease.OutSine);
+            questionCG.DOFade(1, 0.25f).SetEase(Ease.OutSine);
+        }
+    }
+
+    void ChoiceFadeIn()
+    {
+        if(timer > 2f && timer < 3f)
+        {
+            correctRT.transform.localPosition = new Vector3(correctRT.transform.localPosition.x - 20f, correctRT.transform.localPosition.y, 0f);
+            correctRT.DOAnchorPos(correctLocalPos, 0.25f, false).SetEase(Ease.OutSine);
+            correctCG.DOFade(1, 0.25f).SetEase(Ease.OutSine);
+        }
+        if(timer > 3f && timer < 4f)
+        {
+            wrongRT.transform.localPosition = new Vector3(wrongRT.transform.localPosition.x - 20f, wrongRT.transform.localPosition.y, 0f);
+            wrongRT.DOAnchorPos(wrongLocalPos, 0.25f, false).SetEase(Ease.OutSine);
+            wrongCG.DOFade(1, 0.25f).SetEase(Ease.OutSine);
+        }
+    }
+
+    void PromptFadeOut()
+    {
+        questionCG.DOFade(0, 0.25f);
+        if(correctChecker)
+        {  
+            correctRT.DOAnchorPos(new Vector2(0f, 0f), 0.25f, false).SetEase(Ease.OutSine);
+            correctCG.DOFade(0, 1f).SetEase(Ease.InQuint);
+            wrongCG.DOFade(0, 0.25f);
+        }
+        else
+        {
+            correctCG.DOFade(0, 0.25f);
+            wrongRT.DOAnchorPos(new Vector2(0f, 0f), 0.25f, false).SetEase(Ease.OutSine);
+            wrongCG.DOFade(0, 1f).SetEase(Ease.InQuint);
+        }
+    }
+
+    public void SelectRandomScene()
+    {
+        if (QuestionScenes.Count > 0)
+        {
+            RandomIndex = Random.Range(0, QuestionScenes.Count);
+            SelectedSceneName = QuestionScenes[RandomIndex];
+            SceneManager.LoadScene(SelectedSceneName);
+            QuestionScenes.RemoveAt(RandomIndex);
+        }
+        else
+        {
+            Debug.Log("Show Result");
+        }
+        isAnswered = false;
+        timer = 0;
     }
 }
